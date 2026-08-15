e38d502cd1d3e7b59cfcd43c5d1e40ce6c88cf77
Rajendra565 <badatyarajendra186@gmail.com>
2026-08-15 20:35:11 +0530

    Datatype conversion

--- basics/datatype.js
+++ basics/datatype.js
@@ -17,12 +17,28 @@ let accountName = Symbol("Rajendra Kumar");//symbol
 let booleanValue = true; // Boolean
 let bigIntValue=123456789123456789n;//BigInt
 let accountPassword;//Undefined
-console.log(account);
-console.log(nullValue);
-console.log(name);
-console.log(accountName);
-console.log(booleanValue);
-console.log(bigIntValue);
-console.log(accountPassword);
-console.log(typeof account);    
-console.log(typeof nullValue);  
+// console.log(account);
+// console.log(nullValue);
+// console.log(name);
+// console.log(accountName);
+// console.log(booleanValue);
+// console.log(bigIntValue);
+// console.log(accountPassword);
+// console.log(typeof account);    
+// console.log(typeof nullValue);  
+
+// Datatype conversion  
+let score=null
+// console.log(typeof score);
+// console.log(typeof Number(score))
+let score1=Number(score)
+// console.log(score1)
+
+// "33" -> 33
+// "33abe" -> NaN
+// true -> 1    
+// false -> 0
+let isLoggedIn=true;
+console.log(typeof isLoggedIn);
+let isLoggedIn1=Boolean(isLoggedIn)
+console.log(isLoggedIn1)

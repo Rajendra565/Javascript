e507adbfef93874f42c155bbbcbe56f629be7fc2
Rajendra565 <badatyarajendra186@gmail.com>
2026-08-27 23:42:13 +0530

    Add JavaScript array methods

--- 05_Arrays/array.js
+++ 05_Arrays/array.js
@@ -1,1 +1,13 @@
 let arr=[1,2,3,4,5];
+// push() method: The push() method is used to add one or more elements to the end (last position) of an array.
+arr.push(60)
+console.log(arr)
+// pop() method: The pop() method is used to remove one elements to the end (last position) of an array.
+arr.pop()
+console.log(arr)
+// shift() Method  The shift() method is used to remove the first element from an array.
+arr.shift()
+console.log(arr)
+// unshift() Method  The unshift() method is used to add the first element from an array.
+arr.unshift(10,20)
+console.log(arr)
